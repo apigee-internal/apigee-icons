178910662464679d2564f093c911e3727e835ec6
brandon <brandon@behemoth.ca>
2016-09-29 10:47:28 -0700

    Add build system toolkit to repo and source sketch doc

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# icons
-Icon fonts repo for Apigee Edge
+#Apigee Icons
+
+###This library is a self-contained tool for generating icon fonts used by the Apigee platform from a source Sketch document.
+
+
+1. Install the Grunt CLI:
+
+        $ npm install -g grunt-cli
+
+2. Install dependent packages:
+
+        $ npm install
+
+3. Ensure you have the latest version of Sketch installed – Version 40.1 (33804); this tool is dependent on a Sketch CLI tool that is not included in this repo. If you do not have a license for Sketch, the demo version will suffice.
+
+4. Generate icon font assets:
+
+        // `grunt all` will export from sketch and generate icon fonts
+        // `grunt font-only` will regenerate fonts from existing /svg folder
+        // For example:
+
+        $ grunt all --src=apigee.sketch --dst ../
+
+---
+
+> **Important**: The copy of `apigee.sketch` in this repo _may or may not_ be the most recent version of the file. If you're publishing a new release, retrieve the latest copy from `Apigee Dragonfly Dropbox/Apigee Dragonfly Team Folder/Assets/Icons`, generate the font assets, then visually QA each icon using the preview HTML file **before** submitting a PR.
+
---
+++ build/Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+    var src = grunt.option('src')
+    var dst = grunt.option('dst')
+    var path = require('path')
+    require('load-grunt-tasks')(grunt)
+    grunt.initConfig({
+        clean: {
+            icons: {
+                options: {
+                    force: true
+                },
+                src: [
+                    path.join(src, 'svg/*.svg')
+                ]
+            }
+        },
+        shell: {
+            installSketchTool: {
+                command: '/Applications/Sketch.app/Contents/Resources/sketchtool/install.sh'
+            },
+            exportIcons: {
+                command: 'sketchtool export slices "' + path.join(src) + '" --output="' + path.join(dst, 'svg/') + '"'
+            }
+        },
+        webfont: {
+            icons: {
+                src: path.join(dst, 'svg/*.svg'),
+                dest: path.join(dst, 'src/fonts/'),
+                destCss: path.join(dst, 'src/css/'),
+                options: {
+                    font: 'apigee-icons',
+                    fontHeight: 512,
+                    normalize: true,
+                    optimize: false,
+                    destHtml: path.join(dst, 'preview/'),
+                    htmlDemoTemplate: 'template.html',
+                    templateOptions: {
+                        classPrefix: 'apic-'
+                    },
+                    engine: 'node',
+                    verbose: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-webfont');
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.registerTask('all', [ 'clean', 'shell', 'webfont:icons' ]);
+    grunt.registerTask('font-only', [ 'webfont:icons' ]);
+
+};
